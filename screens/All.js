7e1dcdb92804a89630970a874f3245c43025f1ed
unknown <aysamuel007@gmail.com>
2023-03-27 09:19:00 +0200

    firstcommit

---
+++ screens/All.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { StyleSheet, View, Text } from 'react-native';
+
+function All() {
+  return (
+    <View>
+      <Text>All</Text>
+    </View>
+  );
+}
+
+export default All;
---
+++ screens/Tech.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { StyleSheet, View, Text } from 'react-native';
+
+function Tech() {
+  return (
+    <View>
+      <Text>Tech</Text>
+    </View>
+  );
+}
+
+export default Tech;
